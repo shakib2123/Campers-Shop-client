490dd90e97cccdaa2a077466fef2ab3242e44e86
shakib2123 <mshakibalhasan21@gmail.com>
2024-07-10 18:19:53 +0600

    product delete method created

--- src/pages/ProductManagement/ProductManagement.tsx
+++ src/pages/ProductManagement/ProductManagement.tsx
@@ -1,14 +1,5 @@
 import { Button } from "@/components/ui/button";
 import {
-  Dialog,
-  DialogContent,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import {
   Table,
   TableBody,
   TableCell,
@@ -16,55 +7,52 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { useForm } from "react-hook-form";
+import {
+  useDeleteProductMutation,
+  useGetProductsQuery,
+} from "@/redux/api/baseApi";
 import { Link } from "react-router-dom";
-
-const invoices = [
-  {
-    invoice: "INV001",
-    paymentStatus: "Paid",
-    totalAmount: "$250.00",
-    paymentMethod: "Credit Card",
-  },
-  {
-    invoice: "INV002",
-    paymentStatus: "Pending",
-    totalAmount: "$150.00",
-    paymentMethod: "PayPal",
-  },
-  {
-    invoice: "INV003",
-    paymentStatus: "Unpaid",
-    totalAmount: "$350.00",
-    paymentMethod: "Bank Transfer",
-  },
-  {
-    invoice: "INV004",
-    paymentStatus: "Paid",
-    totalAmount: "$450.00",
-    paymentMethod: "Credit Card",
-  },
-  {
-    invoice: "INV005",
-    paymentStatus: "Paid",
-    totalAmount: "$550.00",
-    paymentMethod: "PayPal",
-  },
-  {
-    invoice: "INV006",
-    paymentStatus: "Pending",
-    totalAmount: "$200.00",
-    paymentMethod: "Bank Transfer",
-  },
-  {
-    invoice: "INV007",
-    paymentStatus: "Unpaid",
-    totalAmount: "$300.00",
-    paymentMethod: "Credit Card",
-  },
-];
+import { toast } from "sonner";
+import Swal from "sweetalert2";
 
 const ProductManagement = () => {
+  const { data, isLoading } = useGetProductsQuery(undefined);
+
+  const [deleteProduct] = useDeleteProductMutation();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  console.log(data);
+
+  const handleDelete = (id) => {
+    console.log(id);
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then(async (result: any) => {
+      if (result.isConfirmed) {
+        try {
+          const res = await deleteProduct(id).unwrap();
+          if (res?.success) {
+            Swal.fire({
+              title: "Deleted!",
+              text: "Your product has been deleted.",
+              icon: "success",
+            });
+          }
+        } catch (err) {
+          toast.error("Failed to delete product");
+        }
+      }
+    });
+  };
+
   return (
     <div className="max-w-screen-xl mx-auto py-20 px-3 ">
       <div className="border border-gray-400 rounded-lg p-8">
@@ -88,25 +76,53 @@ const ProductManagement = () => {
               Create A New Product
             </Button>
           </Link>
-          <h2 className="text-xl text-gray-700 font-bold">Total Product: 50</h2>
+          <h2 className="text-xl text-gray-700 font-bold">
+            Total Product: {data?.data?.length}
+          </h2>
         </div>
         <Table className="p-12">
           <TableHeader>
             <TableRow>
-              <TableHead className="w-[100px]">Invoice</TableHead>
-              <TableHead>Status</TableHead>
-              <TableHead>Method</TableHead>
-              <TableHead className="text-right">Amount</TableHead>
+              <TableHead className="w-[100px]">Image</TableHead>
+              <TableHead>Name</TableHead>
+              <TableHead>Price</TableHead>
+              <TableHead>Category</TableHead>
+              <TableHead className="text-right">Actions</TableHead>
             </TableRow>
           </TableHeader>
           <TableBody>
-            {invoices.map((invoice) => (
-              <TableRow key={invoice.invoice}>
-                <TableCell className="font-medium">{invoice.invoice}</TableCell>
-                <TableCell>{invoice.paymentStatus}</TableCell>
-                <TableCell>{invoice.paymentMethod}</TableCell>
+            {data?.data?.map((item) => (
+              <TableRow key={item._id}>
+                <TableCell>
+                  <img
+                    className="object-cover rounded-lg size-14"
+                    src={item.image}
+                    alt={item.name}
+                  />
+                </TableCell>
+                <TableCell className="text-sm">{item.name}</TableCell>
+                <TableCell>{item.price}</TableCell>
+                <TableCell>{item.category}</TableCell>
                 <TableCell className="text-right">
-                  {invoice.totalAmount}
+                  <Button
+                    onClick={() => handleDelete(item._id)}
+                    variant={"destructive"}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="size-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                      />
+                    </svg>
+                  </Button>
                 </TableCell>
               </TableRow>
             ))}
--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -3,6 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000" }),
+  tagTypes: ["Products"],
   endpoints: (builder) => ({
     addProduct: builder.mutation({
       query: (product) => ({
@@ -10,8 +11,29 @@ export const baseApi = createApi({
         method: "POST",
         body: product,
       }),
+      invalidatesTags: ["Products"],
+    }),
+
+    getProducts: builder.query({
+      query: () => ({
+        url: "/products",
+        method: "GET",
+      }),
+      providesTags: ["Products"],
+    }),
+
+    deleteProduct: builder.mutation({
+      query: (id) => ({
+        url: `/products/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["Products"],
     }),
   }),
 });
 
-export const { useAddProductMutation } = baseApi;
+export const {
+  useAddProductMutation,
+  useGetProductsQuery,
+  useDeleteProductMutation,
+} = baseApi;
